7b2699dd0f3b18cbd1b67f920320530e55d770db
Salvador Martínez Landrian <salvadormartinez@Salvadors-MacBook-Pro.local>
2022-06-27 22:03:06 +0200

    Adding ForageApp

---
+++ ForageApp/ForageApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForageApp
+//
+//  Created by Salvador Martínez Landrian on 26/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //MARK: - IBOutlet
+    @IBOutlet weak var cardNumberLabel: UILabel!
+    @IBOutlet weak var cardNumberTextField: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+
+    //MARK: - Cicle of life
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
